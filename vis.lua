bb66b9cb060bd91d532cca679b9bc1684787aab2
Lars Rosengreen <lrosengreen@gmail.com>
2009-11-25 01:36:30 -0800

    Add vis.mask for transparent masks, handle matrix elements sparsely.

--- vis.lua
+++ vis.lua
@@ -9,27 +9,41 @@
 matrix = require "matrix"
 require "gd"
 
+vis = {}
+
 -- Create a grayscale image of the elements of a matrix, and save it
--- as a png to a file.
-local function vis(M, filename)
+-- as a png to a file. Zero elements are white, the lagest elements are black
+function vis.bw(M, filename)
    local rows, columns = M:size()
    local min, max = M:min(), M:max()
    local range = max - min 
    local scale = 255 / range
    local img = assert(gd.createTrueColor(rows, columns))
    local white = img:colorAllocate(255, 255, 255)
-   for i = 1, rows do
-      for j = 1, columns do
-         local e = M[i][j]
-         local color = 255 - math.floor((e - min) * scale)
-         assert(color >= 0 and color <= 255)
-         color = img:colorAllocate(color, color, color)
-         img:setPixel(i - 1, j - 1, color)
+   img:filledRectangle(0, 0, rows - 1, columns - 1, white)
+   for i, v in M:vects() do
+      for j, e in v:elts() do
+         local shade = 255 - math.floor((e - min) * scale)
+         assert(shade >= 0 and shade <= 255)
+         shade = img:colorAllocate(shade, shade, shade)
+         img:setPixel(i - 1, j - 1, shade)
       end
    end
    img:png(filename)
-   return
 end
 
 
-return vis
+function vis.mask(M, fname, maskColor)
+   maskColor = maskColor or {0, 0, 0}
+   local img = gd.create(M:size())
+   local background = img:colorAllocateAlpha(255, 255, 255, 127)
+   local mask = img:colorAllocateAlpha(maskColor[1], 
+                                       maskColor[2], 
+                                       maskColor[3], 0)
+   for i, v in M:vects() do
+      for j, e in v:elts() do
+         img:setPixel(i - 1, j - 1, mask)
+      end
+   end
+   img:png(fname)
+end
